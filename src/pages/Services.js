d1327604cc07c2e0ed010f49edeb247db6865942
Enzo Patti <enzo.patti@aluno.senai.br>
2024-02-05 22:25:30 -0300

    adicionado page de services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { useTheme } from '../ThemeContext';
+import { FiCode, FiLayout, FiServer, FiArrowRight } from 'react-icons/fi';
+
 const Services = () => {
+  const servicesData = [
+    {
+      title: "Ui/Ux Designer",
+    },
+    {
+      title: "Frontend",
+    },
+    {
+      title: "Backend",
+    }
+  ];
+
   const { theme } = useTheme();
+
   return (
     <div className={`${theme === 'dark' ? 'bg-black text-white' : 'bg-slate-50 text-black'}`}>
       <div className="max-w-lg mx-auto" id="services">
@@ -9,6 +24,22 @@ const Services = () => {
           <h1 className="text-4xl font-semibold">Serviços</h1>
           <p className="text-sm font-normal text-slate-600">O que eu ofereço</p>
         </div>
+      </div>
+      <div className="flex justify-center mt-8 space-x-8">
+        {servicesData.map((item, index) => (
+          <div key={index} className="text-center bg-white rounded-lg p-6 shadow-md w-72">
+            <div className="mb-4">
+              {index === 0 && <FiLayout className="text-5xl text-red-500 mx-auto" size={25} />}
+              {index === 1 && <FiCode className="text-5xl text-red-500 mx-auto" size={25} />}
+              {index === 2 && <FiServer className="text-5xl text-red-500 mx-auto" size={25} />}
+            </div>
+            <h2 className="text-2xl font-semibold mb-2">{item.title}</h2>
+            <button className="mt-4 px-4 py-2 text-red-500 bg-transparent rounded-md hover:text-red-600 hover:cursor-pointer focus:outline-none focus:ring focus:border-red-300">
+              Veja Mais
+              <FiArrowRight className="inline ml-2" />
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
